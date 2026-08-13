1e2d2b3c3ebec8eeab08cd0d1e3738337013dc87
Teemo Tebest <teemo.tebest@gmail.com>
2026-08-14 01:12:38 +0300

    Host UNCTAD logo locally instead of hardcoded unctad.org URL (blocks downloads on localhost)

--- src/jsx/components/ChartScatter.jsx
+++ src/jsx/components/ChartScatter.jsx
@@ -69,7 +69,7 @@ function ScatterChart({
         events: {
           load() {
             // eslint-disable-next-line react/no-this-in-sfc
-            this.renderer.image('https://unctad.org/sites/default/files/2022-11/unctad_logo.svg', 5, 15, 80, 100).add();
+            this.renderer.image(`${window.location.href.includes('unctad.org') ? 'https://storage.unctad.org/2023-graphgpt/' : './'}assets/img/unctad_logo.svg`, 5, 15, 80, 100).add();
           }
         },
         height: chartHeight,
